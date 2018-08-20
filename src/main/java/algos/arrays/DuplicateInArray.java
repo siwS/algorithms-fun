bfd16125d46b7b2789c43f72b252cff9825d794c
sofia <tzi.sof@gmail.com>
2018-08-20 23:40:55 +0100

    Change sorting algorithm.

--- src/main/java/algos/arrays/DuplicateInArray.java
+++ src/main/java/algos/arrays/DuplicateInArray.java
@@ -3,7 +3,19 @@ package algos.arrays;
 import algos.search.BinarySearch;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
+/**
+ * Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist.
+ * Assume that there is only one duplicate number, find the duplicate one.
+ *
+ * Restrictions:
+ * O(1) memory
+ * less than O(n^2) complexity
+ * same duplicate can exist multiple times
+ */
 public class DuplicateInArray {
     public int findDuplicate(int[] nums) {
         int sum = 0;
@@ -11,10 +23,16 @@ public class DuplicateInArray {
             sum += num;
         }
 
-        // Quicksort
-        Arrays.sort(nums);
+        // Collections sort uses mergesort ( O(nlogn) )
+        // While Arrays.sort uses quicksort ( O(n^2) )
+        // Mergesort offers stable sorting complexity
+        // While quicksort generally tends to be faster
+        // https://www.cs.auckland.ac.nz/software/AlgAnim/qsort3.html
+        // We choose stable complexity nlogn for purposes of this problem
+        List<Integer> list = Arrays.stream(nums).boxed().sorted().collect(Collectors.toList());
+        nums = list.stream().mapToInt(i->i).toArray();
 
-        // Complexity n * logn due to binary search
+        // Complexity n * log n due to binary search
         int countExists = 0;
         for (int i=1; i < nums.length; i++) {
             int existsIndex = BinarySearch.search(nums, i);
@@ -30,6 +48,6 @@ public class DuplicateInArray {
     public static void main(String[] args) {
         int[] array1 = new int[]{1,3,4,2,2,2};
         int result = new DuplicateInArray().findDuplicate(array1);
-        System.out.print(result);
+        System.out.println(result);
     }
 }
